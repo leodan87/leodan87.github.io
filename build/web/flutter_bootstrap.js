31bf49058eed34fc56632ca2b1f459e8f4bfa0ce
leodan87 <leodan87@users.noreply.github.com>
2026-01-19 03:55:35 -0500

    Build: Compilación web con diseño responsive móvil

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"ef0cd000916d64fa0c5d09cc809fa7ad244a57
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1045578931"
+    serviceWorkerVersion: "2050178115"
   }
 });
